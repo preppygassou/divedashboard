0ee272334e66f6acb9123187b17889434355c8c6
Preppy Gassou <ninopreppy@gmail.com>
2024-12-27 05:56:05 -0300

    last up 6

--- actions/admin.ts
+++ actions/admin.ts
@@ -6,7 +6,8 @@ import { UserRole } from "@prisma/client";
 export const admin = async () => {
   const role = await currentRole();
 
-  if (role === UserRole.ADMIN) {
+ /*  if (role === UserRole.ADMIN) { */
+  if (role === "ADMIN") {
     return { success: "Allowed Server Action!" };
   }
 
